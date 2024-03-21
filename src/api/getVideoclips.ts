9ff4ef8a44b08e9b8a99d4f831a8d3f8ce5d4346
jpascual <jpascual@themindhubcompany.com>
2024-03-21 10:10:31 +0100

    Agregados Videoclips y Predytube

--- src/api/getVideoclips.ts
+++ src/api/getVideoclips.ts
@@ -1,4 +1,4 @@
-export default function getDiscos(): any[] {
+export function getDiscos(): any[] {
   const videoclips: any[] = [
     {
       id: "25143269-58ce-4fb7-ae94-5bad3333ab02",
@@ -8,6 +8,364 @@ export default function getDiscos(): any[] {
       VisualizacionesYoutube: 179,
       descripcion:
         "Tras un periodo inactivo, al final me he decidido y he hecho De tripas, corazón.",
+      featuring: "",
+      beat: "Ramsay Beats",
+      mezcla: "Cansino(PlaymoBeats)",
+      originalLikes: 14,
+    },
+
+    {
+      id: "25143269-58ce-4fb7-ae94-5bad3333ab02",
+      Titulo: "Cuarentrena",
+      thumbnail: "",
+      fechaSalida: "2020-03-28",
+      VisualizacionesYoutube: 994,
+      descripcion:
+        "Desde el confinamiento más extremo llega Cuarentrena un videoclip HomeMade que habla de la situación por la que está pasando el mundo actualmente, como siempre con el toque Predy. Espero que os guste, y no salgáis de casa, ¡¡no me seáis alantaos!!",
+      featuring: "",
+      beat: "Ramsay Beats",
+      mezcla: "Koar en Local12",
+      originalLikes: 79,
+    },
+
+    {
+      id: "25143269-58ce-4fb7-ae94-5bad3333ab02",
+      Titulo: "¿Qué será mañana?",
+      thumbnail: "",
+      fechaSalida: "2019-10-03",
+      VisualizacionesYoutube: 616,
+      descripcion:
+        "¿Que será mañana? ¿Será Futurama? ¿Habrá un mañana? ¡Vete tú a saber! De momento, lo único que sé es que tienes este tema para disfrutarlo eternamente.",
+      featuring: "",
+      beat: "",
+      mezcla: "Koar en Local12",
+      originalLikes: 44,
+    },
+
+    {
+      id: "25143269-58ce-4fb7-ae94-5bad3333ab02",
+      Titulo: "Lo que ellos digan",
+      thumbnail: "",
+      fechaSalida: "2018-11-13",
+      VisualizacionesYoutube: 401,
+      descripcion:
+        "No hagáis caso a lo que digan, sed siempre vosotros mismos... Bueno, a menos que seáis mala persona, ahí si que os toca dejaros aconsejar :P",
+      featuring: "Maria Serra",
+      beat: "Ramsay Beats",
+      mezcla: "Cansino (PlaymoBeats)",
+      originalLikes: 20,
+    },
+
+    {
+      id: "25143269-58ce-4fb7-ae94-5bad3333ab02",
+      Titulo: "Mareado",
+      thumbnail: "",
+      fechaSalida: "2018-07-31",
+      VisualizacionesYoutube: 425,
+      descripcion: "Ya veis como vivo la vida... Mareado.",
+      featuring: "",
+      beat: "RP beats",
+      mezcla: "Cansino (PlaymoBeats)",
+      originalLikes: 27,
+    },
+
+    {
+      id: "25143269-58ce-4fb7-ae94-5bad3333ab02",
+      Titulo: "Oro",
+      thumbnail: "",
+      fechaSalida: "2017-12-20",
+      VisualizacionesYoutube: 1102,
+      descripcion:
+        "Valemos ORO. Pero para que te quieran los demás, quiérete tú primero.",
+      featuring: "",
+      beat: "Ramsay Beats",
+      mezcla: "Cansino (PlaymoBeats)",
+      originalLikes: 79,
+    },
+
+    {
+      id: "25143269-58ce-4fb7-ae94-5bad3333ab02",
+      Titulo: "Conecta2",
+      thumbnail: "",
+      fechaSalida: "2017-06-14",
+      VisualizacionesYoutube: 321,
+      descripcion:
+        "¿Estamos conectados? Este tema va por todos los que me apoyan y por los que me apoyarán en un futuro, gracias a todos.",
+      featuring: "",
+      beat: "",
+      mezcla: "Koar Beats (SoulTeam)",
+      originalLikes: 30,
+    },
+
+    {
+      id: "25143269-58ce-4fb7-ae94-5bad3333ab02",
+      Titulo: "Skate for Life",
+      thumbnail: "",
+      fechaSalida: "2017-02-09",
+      VisualizacionesYoutube: 1351,
+      descripcion:
+        "En mi disco El último titán (2009) dediqué un tema entero a una de mis pasiones, el Skateboard, con la canción Predyflip 360. Los tiempos evolucionan y mi rap también, así que he querido volver a dedicarle otra canción a este deporte, gracias a Dj Xino el tema cuenta con Scratches de Frases del tema inicial (Predyflip 360)",
+      featuring: "Dj Xino",
+      beat: "",
+      mezcla: "Cansino en NPF",
+      originalLikes: 27,
+    },
+
+    {
+      id: "25143269-58ce-4fb7-ae94-5bad3333ab02",
+      Titulo: "Esencia de Gorila",
+      thumbnail: "",
+      fechaSalida: "2016-07-19",
+      VisualizacionesYoutube: 364,
+      descripcion: "",
+      featuring: "",
+      beat: "",
+      mezcla: "",
+      originalLikes: 23,
+    },
+
+    {
+      id: "25143269-58ce-4fb7-ae94-5bad3333ab02",
+      Titulo: "Escucha Piensa Actua",
+      thumbnail: "",
+      fechaSalida: "2016-06-25",
+      VisualizacionesYoutube: 393,
+      descripcion:
+        "Canción realizada con el motivo de las elecciones de España 26J, en la cual se promueve el derecho a votar.",
+      featuring: "",
+      beat: "",
+      mezcla: "Cansino (PlaymoBeats)",
+      originalLikes: 28,
+    },
+
+    {
+      id: "25143269-58ce-4fb7-ae94-5bad3333ab02",
+      Titulo: "Cervantes en Rap",
+      thumbnail: "",
+      fechaSalida: "2016-05-27",
+      VisualizacionesYoutube: 1009,
+      descripcion:
+        "Esta es mi inscripción para el concurso de Cervantes en Rap del festival de almagro",
+      featuring: "",
+      beat: "",
+      mezcla: "",
+      originalLikes: 41,
+    },
+
+    {
+      id: "25143269-58ce-4fb7-ae94-5bad3333ab02",
+      Titulo: "TBP audition d(-_-)b",
+      thumbnail: "",
+      fechaSalida: "2016-03-12",
+      VisualizacionesYoutube: 227,
+      descripcion: "",
+      featuring: "",
+      beat: "",
+      mezcla: "",
+      originalLikes: 17,
     },
   ]
+
+  return videoclips
+}
+
+export function getPredytube(): any[] {
+  const predytube: any[] = [
+    {
+      id: "25143269-58ce-4fb7-ae94-5bad3333ab02",
+      Titulo: "Eso es todo amigos",
+      thumbnail: "",
+      fechaSalida: "2016-04-20",
+      VisualizacionesYoutube: 510,
+      descripcion:
+        "Bueno, ya se ha acabado el proyecto, era lógico ¿no? también por otra parte... Pero no os preocupéis porque voy a seguir subiendo vídeos al canal, pero sin respetar el hecho de sacarlos cada miércoles. Los nuevos vídeos vendrán en función de mi disponibilidad, no olvidéis suscribiros para estar al tanto de mis locuras. Espero que os lo hayáis pasado genial con este proyecto llamado Dyferente y en breves vuelvo con locuras nuevas! :P Un abrazo enorme chavalada!!! :P",
+      featuring: "",
+      beat: "",
+      mezcla: "",
+      originalLikes: 32,
+    },
+
+    {
+      id: "25143269-58ce-4fb7-ae94-5bad3333ab02",
+      Titulo: "El placer de los artistas",
+      thumbnail: "",
+      fechaSalida: "2016-04-13",
+      VisualizacionesYoutube: 802,
+      descripcion:
+        "Y volvemos con mas!, aprovechamos la barbacoa, los amigos, el buen rollo y el cumpleaños de Chucky para grabar un Predytube de como fue nuestra tarde. Este vídeo no hubiera sido posible sin Tacho. Espero que lo disfrutéis y solo me queda mandar un beso a todos  (ya que hoy es el día del Beso) y en especial a los que han participado en el clip, costa, alex, iff, chucky etc...",
+      featuring: "",
+      beat: "",
+      mezcla: "Cansino (Nunca Piso Freno)",
+      originalLikes: 33,
+    },
+
+    {
+      id: "25143269-58ce-4fb7-ae94-5bad3333ab02",
+      Titulo: "Predytube especial versión South Park",
+      thumbnail: "",
+      fechaSalida: "2016-04-06",
+      VisualizacionesYoutube: 719,
+      descripcion:
+        "Hoy toca un vídeo especial, celebrando que ya hemos pasado los 10 Predytubes, he decidido hacer un videoclip en formato South Park.",
+      featuring: "Dj Xino",
+      beat: "",
+      mezcla: "Cansino (PlaymoBeats)",
+      originalLikes: 52,
+    },
+
+    {
+      id: "25143269-58ce-4fb7-ae94-5bad3333ab02",
+      Titulo: "No vas a creer lo que hace Predy en el bosque",
+      thumbnail: "",
+      fechaSalida: "2016-03-30",
+      VisualizacionesYoutube: 599,
+      descripcion:
+        "Un segundo es una foto, de esa idea nace el Predytube de esta semana.",
+      featuring: "Lorena Giner",
+      beat: "",
+      mezcla: "Cansino (PlaymoBeats)",
+      originalLikes: 43,
+    },
+
+    {
+      id: "25143269-58ce-4fb7-ae94-5bad3333ab02",
+      Titulo: "Sacado de la patilla",
+      thumbnail: "",
+      fechaSalida: "2016-03-23",
+      VisualizacionesYoutube: 379,
+      descripcion:
+        "¿Queríais espontaneidad? ¿algo fresco? Pues que mejor que un freestyle, recién sacado de la patilla!!! xD, No es una improvisación perfecta, pero bueno, almenos tiene patas y se va arrastrando por el suelo mientras emite algún que otro sonido.",
+      featuring: "",
+      beat: "IduBeats",
+      mezcla: "",
+      originalLikes: 30,
+    },
+
+    {
+      id: "25143269-58ce-4fb7-ae94-5bad3333ab02",
+      Titulo: "Sopa de Letras",
+      thumbnail: "",
+      fechaSalida: "2016-03-17",
+      VisualizacionesYoutube: 238,
+      descripcion:
+        "El Predytube de esta semana es un lyric vídeo bajo el contexto de un tema buen rollero para sacaros una sonrisa.",
+      featuring: "",
+      beat: "",
+      mezcla: "",
+      originalLikes: 11,
+    },
+
+    {
+      id: "25143269-58ce-4fb7-ae94-5bad3333ab02",
+      Titulo: "¿Aún existe la palabra toyako?",
+      thumbnail: "",
+      fechaSalida: "2016-03-09",
+      VisualizacionesYoutube: 946,
+      descripcion:
+        "¿Aún existe la palabra Toyako? Toyako es una palabra que se utilizaba en los inicios de el Hip-hop para desprestigiar a alguien, (es un adjetivo de Incompetente) el vídeo esta enfocado en un graffiti puesto que es en el ámbito donde se suele utilizar mas este calificativo despectivo.",
+      featuring: "",
+      beat: "",
+      mezcla: "",
+      originalLikes: 34,
+    },
+
+    {
+      id: "25143269-58ce-4fb7-ae94-5bad3333ab02",
+      Titulo: "Estruc tuc tuc tuc turas",
+      thumbnail: "",
+      fechaSalida: "2016-03-02",
+      VisualizacionesYoutube: 560,
+      descripcion:
+        "Esta vez desde Predytube nos hemos enfocado en una parte muy concreta de la música, se trata de la métrica. Así que aquí tenéis un montón de estruc tuc tuc tuc turas! que las disfrutéis!",
+      featuring: "",
+      beat: "Fx M Black Beats",
+      mezcla: "",
+      originalLikes: 28,
+    },
+
+    {
+      id: "25143269-58ce-4fb7-ae94-5bad3333ab02",
+      Titulo: "Dos colgaos + Francia + Flow = Flower ",
+      thumbnail: "",
+      fechaSalida: "2016-02-24",
+      VisualizacionesYoutube: 350,
+      descripcion:
+        "Esta vez os traemos un Predytube algo internacional, desde Francia, concretamente la ciudad de Lyon llega con un estilo mas fresco el videoclip de Flower ambientado en una estética comic para desarrollar este viaje trambolikoh!",
+      featuring: "",
+      beat: "",
+      mezcla: "",
+      originalLikes: 20,
+    },
+
+    {
+      id: "25143269-58ce-4fb7-ae94-5bad3333ab02",
+      Titulo: "20 SONGS TAG CHALLENGE",
+      thumbnail: "",
+      fechaSalida: "2016-02-17",
+      VisualizacionesYoutube: 438,
+      descripcion:
+        "Pues nada, siguiente, el siguiente vídeo se trata de un challenge, el de 20 songs tag, o el tag de las 20 canciones, se trata de revelar un cuestionario de preguntas relacionadas con la música.",
+      featuring: "",
+      beat: "",
+      mezcla: "",
+      originalLikes: 35,
+    },
+
+    {
+      id: "25143269-58ce-4fb7-ae94-5bad3333ab02",
+      Titulo: "Aire",
+      thumbnail: "",
+      fechaSalida: "2016-02-10",
+      VisualizacionesYoutube: 522,
+      descripcion:
+        "En esta ocasión #Predytube os trae algo #Dyferente como siempre, esta vez es la versión melódica del tema de Aire que vio la luz en el año 2013.",
+      featuring: "",
+      beat: "Jay Beat",
+      mezcla: "Cansino (Playmobeats)",
+      originalLikes: 44,
+    },
+
+    {
+      id: "25143269-58ce-4fb7-ae94-5bad3333ab02",
+      Titulo: "GamePlay de Rap en Español",
+      thumbnail: "",
+      fechaSalida: "2016-02-03",
+      VisualizacionesYoutube: 546,
+      descripcion:
+        "Ahora usando un formato nunca antes visto en una canción, se trata de un game play de Rap, en la que tanto el videojuego como el jugador van acorde con la canción del clip.",
+      featuring: "David",
+      beat: "",
+      mezcla: "",
+      originalLikes: 33,
+    },
+    {
+      id: "25143269-58ce-4fb7-ae94-5bad3333ab02",
+      Titulo: "El mejor Thug Life RAP en Español!",
+      thumbnail: "",
+      fechaSalida: "2016-01-27",
+      VisualizacionesYoutube: 1501,
+      descripcion:
+        "Baung!! Predy trae el mejor Gangsta Rap al micro chavales, ¿Qué porqué es el mejor? Porque me lo ha dicho mi abuela.",
+      featuring: "",
+      beat: "California Livin",
+      mezcla: "",
+      originalLikes: 57,
+    },
+
+    {
+      id: "25143269-58ce-4fb7-ae94-5bad3333ab02",
+      Titulo: "Llega Predytube!!!",
+      thumbnail: "",
+      fechaSalida: "2016-02-20",
+      VisualizacionesYoutube: 1922,
+      descripcion:
+        "Vuelve Predy Y eso no es todo amigos!, porque comienza Predytube, cada miércoles tendréis vídeo en este canal así que Suscribiros y uniros a la familia ;)!",
+      featuring: "",
+      beat: "",
+      mezcla: "Cansino (PlaymoBeats)",
+      originalLikes: 69,
+    },
+  ]
+
+  return predytube
 }
