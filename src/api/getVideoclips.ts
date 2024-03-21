f13dad289fe4143bef5dc74e7def19fd1bafaa49
predymaster <predymaster@gmail.com>
2024-03-21 08:25:08 +0100

    add getVideoclips

---
+++ src/api/getVideoclips.ts
@@ -0,0 +1,13 @@
+export default function getDiscos(): any[] {
+  const videoclips: any[] = [
+    {
+      id: "25143269-58ce-4fb7-ae94-5bad3333ab02",
+      Titulo: "De tripas, corazón",
+      thumbnail: "",
+      fechaSalida: "2021-06-16",
+      VisualizacionesYoutube: 179,
+      descripcion:
+        "Tras un periodo inactivo, al final me he decidido y he hecho De tripas, corazón.",
+    },
+  ]
+}
